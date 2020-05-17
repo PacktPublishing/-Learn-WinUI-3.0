c83a2f11fe6cb401baf41c20ffc8adce7d94565b
Alvin Ashcraft <alashcraft@gmail.com>
2020-05-17 12:42:06 -0400

    Add chapter 5 final project
    Adding the completed project for chapter 5.

---
+++ Chapter05/Complete/MyMediaCollection/Views/ItemDetailsPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using MyMediaCollection.ViewModels;
+
+namespace MyMediaCollection.Views
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class ItemDetailsPage : Page
+    {
+        public ItemDetailsPage()
+        {
+            this.InitializeComponent();
+
+            Windows.Storage.ApplicationDataContainer localSettings = Windows.Storage.ApplicationData.Current.LocalSettings;
+            string haveExplainedSaveSetting = localSettings.Values["ItemDetailsHaveExplainedSave"] as string;
+            bool haveExplainedSave = bool.TryParse(haveExplainedSaveSetting, out bool result);
+
+            if (!result || !haveExplainedSave)
+            {
+                SavingTip.IsOpen = true;
+                localSettings.Values["ItemDetailsHaveExplainedSave"] = "true";
+            }
+        }
+
+        public ItemDetailsViewModel ViewModel { get; } = (Application.Current as App)?.Container.GetService<ItemDetailsViewModel>();
+    }
+}
