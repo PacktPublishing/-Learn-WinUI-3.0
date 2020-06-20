ae570698dd6ad6b2d9bd085fa47b9e05f9fd0a04
Alvin Ashcraft <alashcraft@gmail.com>
2020-06-20 14:22:19 -0400

    Add ch8 starter project
    Adding the Start project for ch8.

---
+++ Chapter08/Start/MyMediaCollection/Interfaces/INavigationService.cs
@@ -0,0 +1,10 @@
+﻿namespace MyMediaCollection.Interfaces
+{
+    public interface INavigationService
+    {
+        string CurrentPage { get; }
+        void NavigateTo(string page);
+        void NavigateTo(string page, object parameter);
+        void GoBack();
+    }
+}
