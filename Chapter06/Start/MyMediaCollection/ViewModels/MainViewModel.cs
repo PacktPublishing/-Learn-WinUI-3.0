98d68d70c2cb974d8b49949c1fb084d12588fd05
Alvin Ashcraft <alashcraft@gmail.com>
2020-07-26 08:51:50 -0400

    Remove workarounds from chapter 6
    Remove observablecollection workarounds from chapter 6 code. Remove some extra using statements from the DataService.

--- Chapter06/Start/MyMediaCollection/ViewModels/MainViewModel.cs
+++ Chapter06/Start/MyMediaCollection/ViewModels/MainViewModel.cs
@@ -2,15 +2,16 @@
 using MyMediaCollection.Interfaces;
 using MyMediaCollection.Model;
 using System.Linq;
+using System.Collections.ObjectModel;
 
 namespace MyMediaCollection.ViewModels
 {
     public class MainViewModel : BindableBase
     {
         private string selectedMedium;
-        private TestObservableCollection<MediaItem> items = new TestObservableCollection<MediaItem>();
-        private TestObservableCollection<MediaItem> allItems;
-        private TestObservableCollection<string> mediums;
+        private ObservableCollection<MediaItem> items = new ObservableCollection<MediaItem>();
+        private ObservableCollection<MediaItem> allItems;
+        private ObservableCollection<string> mediums;
         private MediaItem selectedMediaItem;
         private const string AllMediums = "All";
 
@@ -34,9 +35,9 @@ namespace MyMediaCollection.ViewModels
                 items.Add(item);
             }
 
-            allItems = new TestObservableCollection<MediaItem>(Items);
+            allItems = new ObservableCollection<MediaItem>(Items);
 
-            mediums = new TestObservableCollection<string>
+            mediums = new ObservableCollection<string>
             {
                 AllMediums
             };
@@ -49,7 +50,7 @@ namespace MyMediaCollection.ViewModels
             selectedMedium = Mediums[0];
         }
 
-        public TestObservableCollection<MediaItem> Items
+        public ObservableCollection<MediaItem> Items
         {
             get
             {
@@ -61,7 +62,7 @@ namespace MyMediaCollection.ViewModels
             }
         }
 
-        public TestObservableCollection<string> Mediums
+        public ObservableCollection<string> Mediums
         {
             get
             {
