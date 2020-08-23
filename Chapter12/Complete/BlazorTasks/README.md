59a09f944b78bc063b72437afa74277003ba84f7
Alvin Ashcraft <alashcraft@gmail.com>
2020-08-23 14:05:23 -0400

    Chapter 12 - Add completed projects
    Add completed BlazorTasks and BlazorTasksHost projects

---
+++ Chapter12/Complete/BlazorTasks/README.md
@@ -0,0 +1,2 @@
+# BlazorTasks
+ A Blazor WebAssembly site to create and manage tasks.
---
+++ Chapter12/Complete/BlazorTasks/TaskItem.cs
@@ -0,0 +1,8 @@
+namespace BlazorTasks
+{
+    public class TaskItem
+    {
+        public string Name { get; set; }
+        public bool IsComplete { get; set; }
+    }
+}
